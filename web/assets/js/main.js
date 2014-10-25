14b6aeabbb4aac703585d1aa0b695e77e7cccd07
Rusinov Anton <ovvshit@gmail.com>
2014-10-25 14:49:32 +0300

    yes

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -235,22 +235,24 @@
                 return this;
             }
             var el = this,
-                $slide = el.find(".slide"),
+                $img = el.find(".slide img"),
                 $thumb = el.find(".thumbnail"),
                 $controls = el.find(".control-btn"),
-                lastSlide = $slide.length - 1;
+                lastThumb = $thumb.length - 1;
 
             var changeSlide = function(i) {
                 $thumb.removeClass("active");
                 $thumb.eq(i).addClass("active");
-                $slide.removeClass("active");
-                $slide.eq(i).addClass("active");
+                var attr = $thumb.eq(i).find("img").attr("src");
+                $img.attr("src", attr);
+                checkViewport(i);
             }
 
+
             var goPrev = function() {
-                var activeSlide = el.find(".active").index();
+                var activeSlide = el.find(".thumbs .active").index();
                 if (activeSlide == 0) {
-                    activeSlide = lastSlide
+                    activeSlide = lastThumb
                 } else {
                     activeSlide--;
                 }
@@ -258,8 +260,8 @@
             }
 
             var goNext = function() {
-                var activeSlide = el.find(".active").index();
-                if (activeSlide == lastSlide) {
+                var activeSlide = el.find(".thumbs .active").index();
+                if (activeSlide == lastThumb) {
                     activeSlide = 0;
                 } else {
                     activeSlide++;
@@ -274,7 +276,9 @@
                 var i = $(this).index();
                 changeSlide(i);
             });
-
+            $controls.on("dbclick",function(){
+                return
+            });
             $controls.on("click", function() {
                 if ($(this).hasClass('gallery-prev')) {
                     goPrev();
@@ -284,44 +288,143 @@
                     goFullScreen();
                 }
             });
+
+            
+
+            //Scrollbar
+
+            var $area = el.find(".scroll-area"),
+                $bar = el.find(".scrollbar"),
+                $drag = el.find(".scrolldrag"),
+                viewportWidth = el.find(".thumbs").width(),
+                areaWidth, dragWidth;
+
+            var setAreaWidth = function() {
+                var thumbWidth = $thumb.width() + parseInt($thumb.css("border-width")) * 2;
+                areaWidth = $thumb.length * thumbWidth + ($thumb.length - 1) * parseInt($thumb.eq(1).css("margin-left"));
+                $area.css({
+                    "width": areaWidth + "px"
+                });
+
+            }
+
+            var setDragWidth = function() {
+                var q = viewportWidth / areaWidth;
+                dragWidth = el.find(".scrollbar").width() * q;
+                if (dragWidth > el.find(".scrollbar").width()) {
+                    dragWidth = el.find(".scrollbar").width()
+                }
+                dragWidth = Math.round(dragWidth);
+                $drag.css({
+                    "width": dragWidth + "px"
+                });
+
+            }
+
+            var initScroll = function() {
+                setAreaWidth();
+                setDragWidth();
+            }
+
+            initScroll();
+
+            $drag.draggable({
+                axis: "x",
+                containment: "parent",
+                addClasses: false,
+                cursor: 'pointer',
+                stop: function() {
+                    dragGoing = false;
+                    changeDrag();
+                }
+            });
+
+            var dragEndPos = $bar.width() - dragWidth,
+                areaEndPos = -(areaWidth - viewportWidth),
+                movCoef = areaEndPos / dragEndPos,
+                dragHover = false,
+                dragGoing = false;
+
+            var changeDrag = function() {
+                if (dragHover || dragGoing) {
+                    $drag.addClass('hover')
+                } else {
+                    $drag.removeClass('hover')
+                }
+            }
+
+            $drag.on("drag", function() {
+                var dragLeftPos = parseInt($drag.css('left')),
+                    areaPos = Math.round(dragLeftPos * movCoef);
+                $area.css({
+                    'left': areaPos + 'px'
+                });
+                dragGoing = true;
+                changeDrag();
+            });
+
+            $drag.on('mouseenter', function() {
+                dragHover = true;
+                changeDrag();
+            });
+
+            $drag.on('mouseleave', function() {
+                dragHover = false;
+                changeDrag();
+            });
+
+            var checkViewport = function(i) {
+                var areaPos = parseInt($area.css("left")),
+                    thumbWidth = $thumb.width() + parseInt($thumb.css("border-width")) * 2 + parseInt($thumb.eq(1).css("margin-left")),
+                    slidesInViewport = Math.round((viewportWidth + 30) / thumbWidth),
+                    firstVisible = Math.round(-areaPos / thumbWidth) + 1,
+                    lastVisible = firstVisible + slidesInViewport - 1,
+                    toLastThumb = -(areaWidth - viewportWidth);
+                    if (areaPos % thumbWidth != 0) {
+                      areaPos = thumbWidth * parseInt(areaPos / thumbWidth); 
+                    }
+                i++;
+              
+                if (i >= firstVisible && i <= lastVisible) {
+
+                } else {
+                    if (i == lastThumb + 1) {
+                        $area.stop().animate({
+                            "left": toLastThumb + "px"
+                        }, 200);
+                        areaPos = toLastThumb;
+                    } else if (i === 1) {
+                        $area.stop().animate({
+                            "left": "0px"
+                        }, 200);
+                        areaPos = 0;
+                    } else {
+                        if (Math.abs(i - firstVisible) < Math.abs(i - lastVisible)) {
+                            $area.stop().animate({
+                                "left": areaPos + thumbWidth + "px"                                
+                            }, 200);
+                            areaPos = areaPos + thumbWidth;
+                        } else {
+                            $area.stop().animate({
+                                "left": areaPos - thumbWidth + "px"                                
+                            }, 200);
+                            areaPos = areaPos - thumbWidth;
+                        }
+                    }
+
+                }
+                
+               areaDrag(areaPos);
+            }
+
+            var areaDrag = function(p){
+                var areaPos = p,
+                 dragLeftPos = Math.round( areaPos / movCoef);
+                 $drag.stop().animate({left:dragLeftPos+"px"}, 100);
+            }
         }
     })(jQuery);
 
-
-;
-document.onmousemove = mouseMove;
-
-function mouseMove(ev) {
-    ev = ev || window.event;
-    var mousePos = mouseCoords(ev);
-    console.log(mousePos.x, mousePos.y)
-}
-
-function mouseCoords(ev) {
-    if (ev.pageX || ev.pageY) {
-        return {
-            x: ev.pageX,
-            y: ev.pageY
-        };
-    }
-    return {
-        x: ev.clientX + document.body.scrollLeft - document.body.clientLeft,
-        y: ev.clientY + document.body.scrollTop - document.body.clientTop
-    };
-};
-(function($) {
-    $.fn.scrollBar = function() {
-        if (this.length == 0) return this;
-        if (this.length > 1) {
-            this.each(function() {
-                $(this).scrollBar(options)
-            });
-            return this;
-        }
-
-        
-    }
-});
 
 ;
 (function($) {
